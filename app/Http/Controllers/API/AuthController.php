5e35d860155dc3b54f6eae22047ad08ebd039276
ito <ito@argyle.jp>
2022-12-18 08:49:24 +0900

    編集

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -13,35 +13,39 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
-            'customer_name' => 'required|max:55',
-            'owner_name' => 'required|max:55',
-            'key' => 'required|max:55',
-            'name' => 'required|max:55',
-            'url' => 'required',
-            'remarks' => 'max:55',
+        // $validatedData = $request->validate([
+        //     'customer_name' => 'required|max:55',
+        //     'owner_name' => 'required|max:55',
+        //     'key' => 'required|max:55',
+        //     'name' => 'required|max:55',
+        //     'url' => 'required',
+        //     'remarks' => 'max:55',
 
 
-        ]);
-
-
-
-         $project_code = "PAB01";
-        $service_code = "UYZ99";
-         $validatedData['project_code'] = $project_code;
-         $validatedData['service_code'] = $service_code;
-
-        //    $validatedData['password'] = bcrypt("s3cretpass");
-        //    $validatedData['user'] = "sample-user_1";
-              $validatedData['user'] = "sample";
-              $validatedData['password'] = bcrypt("s3cret");
-        $user = User::create($validatedData);
-
-        $accessToken = $user->createToken('authToken')->accessToken;
+        // ]);
 
 
 
-        return response()->json(['project_code' => $project_code,'service_code' => $service_code],Response::HTTP_CREATED);
+        //  $project_code = "PAB01";
+        // $service_code = "UYZ99";
+        //  $validatedData['project_code'] = $project_code;
+        //  $validatedData['service_code'] = $service_code;
+
+        // //    $validatedData['password'] = bcrypt("s3cretpass");
+        // //    $validatedData['user'] = "sample-user_1";
+        //       $validatedData['user'] = "sample";
+        //       $validatedData['password'] = bcrypt("s3cret");
+        // $user = User::create($validatedData);
+
+        // $accessToken = $user->createToken('authToken')->accessToken;
+
+
+
+        // return response()->json(['project_code' => $project_code,'service_code' => $service_code],Response::HTTP_CREATED);
+         return response()->json([
+        'project_code' => 'PAB01',
+        "service_code" => "UYZ99"
+    ],Response::HTTP_CREATED);
     }
 
     public function login(Request $request)
