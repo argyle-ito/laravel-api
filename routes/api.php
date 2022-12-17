1eb40d8e0c74568e796986647c736b26dc4b6d21
ito <ito@argyle.jp>
2022-12-17 17:17:17 +0900

    編集

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/projects',[App\Http\Controllers\API\AuthController::class,'register'])->middleware('auth:api');
+Route::post('/projects',[App\Http\Controllers\API\AuthController::class,'register']);
 Route::post('/auth', [App\Http\Controllers\API\AuthController::class,'login']);
 Route::apiResource('api/ceo', 'App\Http\Controllers\API\CEOController')->middleware('auth:api');
 
 
     Route::get('UYZ99/conf', [App\Http\Controllers\API\ProjectController::class,'getSetting'])->middleware('auth:api');
     Route::put('UYZ99/conf', [App\Http\Controllers\API\ProjectController::class,'putSetting'])->middleware('auth:api');
-    Route::post('UYZ99/jobs.json', [App\Http\Controllers\API\ProjectController::class,'updateGob'])->middleware('auth:api');
+    Route::post('UYZ99/jobs.json', [App\Http\Controllers\API\ProjectController::class,'updateGob']);
     Route::get('UYZ99/jobs', [App\Http\Controllers\API\ProjectController::class,'getGob'])->middleware('auth:api');
     Route::get('UYZ99/status', [App\Http\Controllers\API\ProjectController::class,'status'])->middleware('auth:api');
     Route::get('UYZ99/stats/{year}/{month}', [App\Http\Controllers\API\ProjectController::class,'info'])->middleware('auth:api');
