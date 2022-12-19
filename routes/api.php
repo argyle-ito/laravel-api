c079155937be86f85c7868da6e392255c02554da
ito <ito@argyle.jp>
2022-12-19 09:03:34 +0900

    編集

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/projects',[App\Http\Controllers\API\AuthController::class,'register']);
 Route::post('/auth', [App\Http\Controllers\API\AuthController::class,'login']);
-Route::apiResource('api/ceo', 'App\Http\Controllers\API\CEOController')->middleware('auth:api');
+// Route::apiResource('api/ceo', 'App\Http\Controllers\API\CEOController')->middleware('auth:api');
 
 
-    Route::get('UYZ99/conf', [App\Http\Controllers\API\ProjectController::class,'getSetting'])->middleware('auth:api');
-    Route::put('UYZ99/conf', [App\Http\Controllers\API\ProjectController::class,'putSetting'])->middleware('auth:api');
+    Route::get('UYZ99/conf', [App\Http\Controllers\API\ProjectController::class,'getSetting']);
+    Route::put('UYZ99/conf', [App\Http\Controllers\API\ProjectController::class,'putSetting']);
     Route::post('UYZ99/jobs.json', [App\Http\Controllers\API\ProjectController::class,'updateGob']);
-    Route::get('UYZ99/jobs', [App\Http\Controllers\API\ProjectController::class,'getGob'])->middleware('auth:api');
-    Route::get('UYZ99/status', [App\Http\Controllers\API\ProjectController::class,'status'])->middleware('auth:api');
-    Route::get('UYZ99/stats/{year}/{month}', [App\Http\Controllers\API\ProjectController::class,'info'])->middleware('auth:api');
-
-    Route::delete('PAB01', [App\Http\Controllers\API\ProjectController::class,'deleteProject'])->middleware('auth:api');
-    Route::get('UYZ99/{filename}.csv', [App\Http\Controllers\API\ProjectController::class,'getCsv'])->middleware('auth:api');
-    Route::put('UYZ99/{filename}.csv', [App\Http\Controllers\API\ProjectController::class,'postCsv'])->middleware('auth:api');
+    Route::get('UYZ99/jobs', [App\Http\Controllers\API\ProjectController::class,'getGob']);
+    Route::get('UYZ99/status', [App\Http\Controllers\API\ProjectController::class,'status']);
+    Route::get('UYZ99/stats/{year}/{month}', [App\Http\Controllers\API\ProjectController::class,'info']);
+    Route::delete('PAB01', [App\Http\Controllers\API\ProjectController::class,'deleteProject']);
+    Route::get('UYZ99/{filename}.csv', [App\Http\Controllers\API\ProjectController::class,'getCsv']);
+    Route::put('UYZ99/{filename}.csv', [App\Http\Controllers\API\ProjectController::class,'postCsv']);
